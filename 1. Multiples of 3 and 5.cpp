0d5be650b8e16f1909120bf4e44658e7ba205df6
mayankKumar64ce <mayank_encom64@yahoo.com>
2020-01-01 11:29:05 +0530

    3rd solved
    3rd solved again

--- 1. Multiples of 3 and 5.cpp
+++ 1. Multiples of 3 and 5.cpp
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 #include <bits/stdc++.h>
 
 using namespace std;
@@ -9,16 +10,29 @@ int main() {
     while(t--)
     {
         scanf("%lld",&n);
-        ll sum=0;
+        n--;
         ll a=n/3,b=n/5,c=n/15;
-        sum+=3*(a*(a+1)/2)+5*(b*(b+1)/2)-15*(c*(c+1)/2);
-        if((n%3==0) or (n%5==0))
-        {
-            sum-=n;
-        }
+        ll sum=3*(a*(a+1)/2)+5*(b*(b+1)/2)-15*(c*(c+1)/2);
         printf("%lld\n",sum);
     }
-    
-    
     return 0;
+=======
+#include <bits/stdc++.h>
+
+using namespace std;
+#define ll long long
+
+int main() {
+    ll t,n;
+    scanf("%lld",&t);
+    while(t--)
+    {
+        scanf("%lld",&n);
+        n--;
+        ll a=n/3,b=n/5,c=n/15;
+        ll sum=3*(a*(a+1)/2)+5*(b*(b+1)/2)-15*(c*(c+1)/2);
+        printf("%lld\n",sum);
+    }
+    return 0;
+>>>>>>> 3a0c7f85773a69b4f2dc98e003ccdda499222675
 }
